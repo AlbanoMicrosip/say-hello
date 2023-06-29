a2ae61b654cbe01c46d24daa467918aacd2ec818
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-28 18:31:49 -0600

    first commit

---
+++ src/test/java/com/sayhello/say/SayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayhello.say;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
