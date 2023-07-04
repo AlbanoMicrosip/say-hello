b3b71244d918f917b090a8f1f984373e6f4dfea8
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 18:09:22 -0600

    Volviendo a configuracion basica

--- src/main/java/com/sayhello/say/SayApplication.java
+++ src/main/java/com/sayhello/say/SayApplication.java
@@ -1,19 +1,9 @@
 package com.sayhello.say;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 @RestController
 @SpringBootApplication
